70d8f289c21a07e967087b6dd35ced0a87d104ca
xLinka <Oliver.dunnington@outlook.com>
2023-07-10 17:00:41 +0100

    Start

---
+++ PiShockApi/PiShockApi/Operations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiShockApi
+{
+    public class Operations
+    {
+        public class ShockCommand
+        {
+            public string? Username { get; set; }
+            public string? Apikey { get; set; }
+            public string? Code { get; set; }
+            public string? Name { get; set; }
+            public int Op { get; set; }
+            public int Duration { get; set; }
+            public int Intensity { get; set; }
+        }
+
+        public class VibrateCommand
+        {
+            public string? Username { get; set; }
+            public string? Apikey { get; set; }
+            public string? Code { get; set; }
+            public string? Name { get; set; }
+            public int Op { get; set; }
+            public int Duration { get; set; }
+            public int Intensity { get; set; }
+        }
+
+        public class BeepCommand
+        {
+            public string? Username { get; set; }
+            public string? Apikey { get; set; }
+            public string? Code { get; set; }
+            public string? Name { get; set; }
+            public int Op { get; set; }
+            public int Duration { get; set; }
+        }
+    }
+}
---
+++ PiShockApi/PiShockApi/PiShockApiClient.cs
@@ -0,0 +1,31 @@
+﻿using static PiShockApi.Operations;
+using System.Net.Http.Json;
+
+public class PiShockApiClient
+{
+    private readonly HttpClient client;
+
+    public PiShockApiClient()
+    {
+        client = new HttpClient();
+        client.BaseAddress = new Uri("https://do.pishock.com/api/");
+    }
+
+    public async Task<string> SendShockCommand(ShockCommand command)
+    {
+        var response = await client.PostAsJsonAsync("apioperate/", command);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<string> SendVibrateCommand(VibrateCommand command)
+    {
+        var response = await client.PostAsJsonAsync("apioperate/", command);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<string> SendBeepCommand(BeepCommand command)
+    {
+        var response = await client.PostAsJsonAsync("apioperate/", command);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
